9b4d0dd019b949de3b10e528aad65b8def32cfba
volkovasystems <richeve.bebedor@gmail.com>
2017-06-02 02:31:44 +0800

    Finished erode procedure.

---
+++ erode-test.js
@@ -0,0 +1,13 @@
+
+const assert = require( "assert" );
+const erode = require( "./erode.js" );
+
+let symbol = Symbol( "hello" );
+let data = { };
+data[ symbol ] = 12345;
+
+erode( symbol, data );
+
+assert.equal( data[ symbol ], undefined, "should be equal" );
+
+console.log( "ok" );
